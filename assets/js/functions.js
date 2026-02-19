51d83a2b32b5a0087488bb6aa12751b444e1d1e0
chriko3 <christoph.kohout03@gmail.com>
2026-02-19 19:02:39 +0100

    adjust

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -41,7 +41,7 @@ function renderLoveBox2(){
     const diffDays = Math.abs(Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24)));
     mainContent.innerHTML += elementNormalTemplate(`${diffDays} Tage 📆`);
 
-    const mapsIframe = `<iframe 
+    const mapsIframe = `<iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d810.4403797459418!2d16.72002487644746!3d48.34228647153582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sde!2sat!4v1771506364053!5m2!1sde!2sat" 
         width="100%" 
         height="250" 
@@ -51,7 +51,7 @@ function renderLoveBox2(){
         referrerpolicy="no-referrer-when-downgrade">
     </iframe>`;
 
-    mainContent.innerHTML += elementNormalTemplate(mapsIframe);
+    mainContent.innerHTML += elementNormalTemplate('Erstes Date 👫🏼' + mapsIframe);
     const elements = mainContent.querySelectorAll('.element');
     const lastElement = elements[elements.length - 1];
     lastElement.classList.add('full-width');
